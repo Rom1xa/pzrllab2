7b2c430b1e58f511b04f6c3471a37427f89fcab8
Rom1xa <abakumoff.roman2018@yandex.ru>
2023-12-15 13:12:53 +0300

    feat: added input reading

--- main.c
+++ main.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+int main(int argc, char const *argv[]) {
+    size_t bufsize = 128;
+    char* buf = malloc(bufsize * sizeof(char));
+    size_t len = 0;
+    char c;
+
+    printf("Enter expression: ");
+    do {
+        c = getchar();
+        if (!isspace(c)) {
+            buf[len++] = c;
+        } else if (len + 1 >= bufsize) {
+            char* tmp = realloc(buf, bufsize * 2 * sizeof(char));
+            if (tmp == NULL) {
+                fprintf(stderr, "Error: Not enough memory.\n");
+                exit(-1);
+            }
+            buf = tmp;
+        }
+    } while (c != '\n');
+
+    buf[len++] = '\0';
+
+    free(buf);
+    return 0;
+}
